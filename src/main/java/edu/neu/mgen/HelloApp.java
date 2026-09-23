0984b9d25c6405f8754b95d8073ee9dd783ef764
Yashaaditi <yashaadititeki@gmail.com>
2026-09-23 14:51:47 -0700

    Initial commit

---
+++ src/main/java/edu/neu/mgen/HelloApp.java
@@ -0,0 +1,13 @@
+package edu.neu.mgen;
+
+// This is my first Java program. Really excited to learn more in the class!
+/*
+* This is a simple Java program to print a message that shows that this is my first Java program.   
+* From the lecture I learned about debugging and how a minor syntax error can change the whole program. 
+*/
+public class HelloApp { 
+    public static void main(String[] args) {
+        System.out.println("It is my first Java program");
+    }
+    
+}
